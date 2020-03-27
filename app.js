9ef7b49023c9e09b2257acd676ec99dd3b2d3df8
Kira <kirahaxor20@gmail.com>
2020-03-27 20:39:46 +0700

    fix app get

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
+const path = require('path');
+
 
 const app = express()
 const basicAuth = require('express-basic-auth')
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 app.use(basicAuth({
     users: { 'kira': 'mantapbanget' }
 }))
+
+app.get('/', (req, res) => res.send({
+    'status': 0,
+    'response': {
+        'path': '/api/v1/no-rek',
+        'methode': 'POST',
+        'auth': 'Basic a2lyYTptYW50YXBiYW5nZXQ=',
+        'body': {
+            'kodeBank': ' ',
+            'noRek': ' '
+        }
+    },
+    'description': 'nothings here'
+}))
   
+app.get('/api/v1/no-rek', (req, res) => res.send({
+    'status': 0,
+    'response': {
+        'methode': 'POST',
+        'auth': 'Basic a2lyYTptYW50YXBiYW5nZXQ=',
+        'body': {
+            'kodeBank': ' ',
+            'noRek': ' '
+        }
+    },
+    'description': '405 Method Not Allowed'
+}))
+
 app.post('/api/v1/no-rek', async (req, res) => {
     const params = {
         "kodeBank": req.body.kodeBank,
