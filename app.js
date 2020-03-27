728279cf27423dd460a9c2559205a60f93b513d0
Kira <kirahaxor20@gmail.com>
2020-03-27 18:34:26 +0700

    init

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+
+const app = express()
+const basicAuth = require('express-basic-auth')
+app.use(bodyParser.json())
+app.use(basicAuth({
+    users: { 'kira': 'mantapbanget' }
+}))
+
+app.get('/', function (req, res) {
+    res.send('hola')
+})
+  
+app.post('/api/v1/no-rek', async (req, res) => {
+    const params = {
+        "kodeBank": req.body.kodeBank,
+        "noRek": req.body.noRek
+    }
+
+    request(`https://api.makira.id/cek-norek?kodeBank=${params.kodeBank}&noRek=${params.noRek}`, function (error, response, body) {
+        const resp = JSON.parse(body)
+        res.send(resp)
+    })
+})
+
+const port = 2020
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
